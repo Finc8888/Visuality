a90754032d623ab62603d13a776499cda5312b9d
Finc8888 <finc88@gmail.com>
2020-10-16 22:49:44 +0300

    feature(src):Adding nextjsgl

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,42 @@
 console.log('Init');
+// @ts-ignore
+import jsgl from 'nextjsgl';
+
+
+/* Instantiate JSGL Panel. */
+const myPanel = new jsgl.Panel(document.getElementById("panel"));
+
+/* Start drawing! */
+
+/* Create circle and modify it */
+const circle = myPanel.createCircle();
+circle.setCenterLocationXY(50,50);
+circle.setRadius(30);
+circle.getStroke().setWeight(5);
+circle.getStroke().setColor("rgb(255,0,0)");
+myPanel.addElement(circle);
+
+/* Create polygon and modify it */
+const polygon = myPanel.createPolygon();
+polygon.addPointXY(50,50);
+polygon.addPointXY(110,50);
+polygon.addPointXY(150,80);
+polygon.addPointXY(110,110);
+polygon.addPointXY(50,110);
+polygon.getStroke().setWeight(5);
+polygon.getStroke().setColor("rgb(0,0,255)");
+polygon.getFill().setColor("rgb(0,255,0)");
+polygon.getFill().setOpacity(0.5);
+myPanel.addElement(polygon);
+
+/* Create text label and modify it */
+const label = myPanel.createLabel();
+label.setText("Hello World!");
+label.setLocationXY(30,120);
+label.setBold(true);
+label.setFontFamily("sans-serif");
+label.setFontSize(24);
+myPanel.addElement(label);
 
 interface Vector2D {
     x: number;
