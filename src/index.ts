478deef0c9bd1789b68f720e267bb33e969f7706
Finc8888 <finc88@gmail.com>
2020-10-20 09:15:11 +0300

    feature(src):jsgl integratioin

--- src/index.ts
+++ src/index.ts
@@ -2,57 +2,12 @@ console.log('Init');
 // @ts-ignore
 import jsgl from 'nextjsgl';
 
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('jsgl',jsgl);
+    /* Instantiate JSGL Panel. */
+    const myPanel = new jsgl.Panel(document.getElementById("panel"));
+});
 
-/* Instantiate JSGL Panel. */
-const myPanel = new jsgl.Panel(document.getElementById("panel"));
 
 /* Start drawing! */
-
-/* Create circle and modify it */
-const circle = myPanel.createCircle();
-circle.setCenterLocationXY(50,50);
-circle.setRadius(30);
-circle.getStroke().setWeight(5);
-circle.getStroke().setColor("rgb(255,0,0)");
-myPanel.addElement(circle);
-
-/* Create polygon and modify it */
-const polygon = myPanel.createPolygon();
-polygon.addPointXY(50,50);
-polygon.addPointXY(110,50);
-polygon.addPointXY(150,80);
-polygon.addPointXY(110,110);
-polygon.addPointXY(50,110);
-polygon.getStroke().setWeight(5);
-polygon.getStroke().setColor("rgb(0,0,255)");
-polygon.getFill().setColor("rgb(0,255,0)");
-polygon.getFill().setOpacity(0.5);
-myPanel.addElement(polygon);
-
-/* Create text label and modify it */
-const label = myPanel.createLabel();
-label.setText("Hello World!");
-label.setLocationXY(30,120);
-label.setBold(true);
-label.setFontFamily("sans-serif");
-label.setFontSize(24);
-myPanel.addElement(label);
-
-interface Vector2D {
-    x: number;
-    y: number;
-}
-
-    function calculateLength(v: Vector2D) {
-        return Math.sqrt(v.x * v.x + v.y * v.y);
-    }
-
-    interface NamedVector {
-        name: string;
-        x: number;
-        y: number;
-    }
-
-const v: NamedVector = { x: 3, y: 4, name: 'Zee' };
-console.log(calculateLength(v)); // ok, результат равен 5.
-
+console.log('jsgl2',jsgl);
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,31 @@
 const path = require('path');
 
+// module.exports = {
+//   entry: './src/index.ts',
+//   devtool: 'inline-source-map',
+//   module: {
+//     rules: [
+//       {
+//         test: /\.tsx?$/,
+//         use: 'ts-loader',
+//         exclude: /node_modules/,
+//       },
+//     ],
+//   },
+//   resolve: {
+//     extensions: [ '.tsx', '.ts', '.js' ],
+//   },
+//   output: {
+//     filename: 'script.js',
+//     path: path.resolve(__dirname, 'dist'),
+//   },
+// };
+
+
 module.exports = {
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
-  },
+  entry: './src/index.js',
   output: {
-    filename: 'script.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-};
+    filename: 'script.js',
+  }
+}
